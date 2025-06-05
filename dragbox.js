7d2a3c7cec97fc2c75b04f3068eb4ed3c2b2aef0
MBB200291 <MBB200291@users.noreply.github.com>
2025-06-05 22:11:08 +0800

    fix dragbox fail to close issue

--- dragbox.js
+++ dragbox.js
@@ -11,100 +11,91 @@
       document.onmousedown = null;
     }
   });
+
   function dragBoxToDownload(evtD) {
-    var startx = evtD.pageX;
-    var starty = evtD.pageY;
-    var div = document.createElement("div");
+    let startx = evtD.pageX;
+    let starty = evtD.pageY;
+    let div = document.createElement("div");
     div.className = "tempDiv";
-    div.style.left = evtD.pageX + "px";
-    div.style.top = evtD.pageY + "px";
     div.style.cssText =
-      "border: 1px dashed blue;  background: #80ff00;  position: absolute;  width: 0; height: 0;  opacity: 0.1;";
+      "border: 1px dashed blue; background: #80ff00; position: absolute; width: 0; height: 0; opacity: 0.1; pointer-events: none;";
+    div.style.left = startx + "px";
+    div.style.top = starty + "px";
     document.body.appendChild(div);
-    document.onmousemove = function (evtM) {
+
+    function cleanup() {
+      if (div && div.parentNode) {
+        div.parentNode.removeChild(div);
+      }
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+
+    function onMouseMove(evtM) {
       div.style.left = Math.min(evtM.pageX, startx) + "px";
       div.style.top = Math.min(evtM.pageY, starty) + "px";
       div.style.width = Math.abs(startx - evtM.pageX) + "px";
       div.style.height = Math.abs(starty - evtM.pageY) + "px";
+    }
 
-      document.onmouseup = function (evtU) {
-        endx = evtU.pageX;
-        endy = evtU.pageY;
-        div.parentNode.removeChild(div);
-        document.onmousemove = null;
-        document.onmouseup = null;
-        var dragbox = {
-          left: Math.min(startx, endx),
-          top: Math.max(starty, endy),
-          right: Math.max(startx, endx),
-          bottom: Math.min(starty, endy),
-        };
-        console.log(dragbox);
-        downloadBatch(filter(getDownloadableElements(), dragbox));
+    function onMouseUp(evtU) {
+      let endx = evtU.pageX;
+      let endy = evtU.pageY;
+
+      let dragbox = {
+        left: Math.min(startx, endx),
+        top: Math.max(starty, endy),
+        right: Math.max(startx, endx),
+        bottom: Math.min(starty, endy),
       };
-    };
+      console.log(dragbox);
+      cleanup();
+      downloadBatch(filter(getDownloadableElements(), dragbox));
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
   }
 
-  // function to get element locaiton (client coodinate)
-  // source: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
   function getOffset(el) {
     const rect = el.getBoundingClientRect();
     return {
       left: rect.left + window.scrollX,
       top: rect.top + window.scrollY,
-      right: rect.right + +window.scrollX,
+      right: rect.right + window.scrollX,
       bottom: rect.bottom + window.scrollY,
     };
   }
 
-  // get all downloadable elements in page and get relative position
   function getDownloadableElements() {
     var downloadableElements = document.querySelectorAll(
       'a[href$=".gif"], a[href$=".pdf"], a[href$=".ppt"], a[href$=".jpg"]'
     );
-    console.log("douwnloadable elements >>>");
-    console.log(downloadableElements);
+    console.log("downloadable elements >>>", downloadableElements);
     return downloadableElements;
   }
 
-  // checkt postion in rectangle
   function checkInside(offset1, offset2) {
-    if (offset1.left > offset2.left && offset1.left < offset2.right) {
-      if (
-        (offset1.top > offset2.bottom && offset1.top < offset2.top) ||
-        (offset1.bottom > offset2.bottom && offset1.bottom < offset2.top)
-      ) {
-        return true;
-      }
-    } else if (offset1.right > offset2.left && offset1.right < offset2.right) {
-      if (
-        (offset1.top > offset2.bottom && offset1.top < offset2.top) ||
-        (offset1.bottom > offset2.bottom && offset1.bottom < offset2.top)
-      ) {
-        return true;
-      }
-    } else {
-      return false;
-    }
-  }
-  function filter(downloadableElements, dragbox) {
-    var selectedElements = [];
-    for (e of downloadableElements) {
-      var position = getOffset(e);
-      var inside = checkInside(position, dragbox);
-      if (inside) {
-        selectedElements.push(e);
-        console.log(position);
-      }
-    }
-    return selectedElements;
+    return (
+      ((offset1.left > offset2.left && offset1.left < offset2.right) ||
+        (offset1.right > offset2.left && offset1.right < offset2.right)) &&
+      ((offset1.top > offset2.bottom && offset1.top < offset2.top) ||
+        (offset1.bottom > offset2.bottom && offset1.bottom < offset2.top))
+    );
   }
 
-  // triger batch doownload
+  function filter(downloadableElements, dragbox) {
+    return Array.from(downloadableElements).filter((e) => {
+      const pos = getOffset(e);
+      const inside = checkInside(pos, dragbox);
+      if (inside) console.log(pos);
+      return inside;
+    });
+  }
+
   function downloadBatch(selectedElements) {
-    for (e of selectedElements) {
+    for (const e of selectedElements) {
       console.log("download..." + e);
-      // window.open(e.href);
       downloadURI(e.href);
     }
   }
